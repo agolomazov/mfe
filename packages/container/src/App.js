aeecf5bebbc809a7e6b38f3bf9472b069800f518
Голомазов Антон Васильевич <golomazov.av@esphere.ru>
2021-03-13 13:34:09 +0300

    Add randomize css styles for production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,11 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co'
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -161,7 +161,7 @@ export default function Pricing() {
           color="textSecondary"
           component="p"
         >
-          Quickly build an effective pricing table for your potential customers
+          <RouterLink to="/">Quickly build</RouterLink> an effective pricing table for your potential customers
           with this layout. It&apos;s built with default Material-UI components
           with little customization.
         </Typography>
